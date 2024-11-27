aea3ac4dececc3bf571f67079edb2b2ce81baddf
srinivas kannan <srinivaskannan22@gmail.com>
2024-11-27 21:40:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+function handleClick(event) {
+    const element = event.target.innerText; // Get the text content of the clicked element
+    const inputElement = document.querySelector(".input");
+    inputElement.addEventListener("keydown",(event)=>{
+        if(event.key==="Enter"){
+            inputElement.value!=''?
+            inputElement.value=eval(inputElement.value):inputElement.value="0";
+        }
+    }) 
+    // Select the input element
+
+    // If "C" is clicked, clear the input
+    if (element === "C") {
+        inputElement.value = ""; // Clear the input field
+    } 
+    // If "=" is clicked, evaluate the expression
+    else if (element === "=") {
+        try {
+            inputElement.value!=""?
+            inputElement.value = eval(inputElement.value):
+            inputElement.value="0"; // Evaluate the input content
+        } catch {
+            inputElement.value = "Error"; // Display error if evaluation fails
+        }
+    } 
+    else if (element=='x'){
+        inputElement.value+='*'
+    }
+    // Otherwise, append the clicked value to the input field
+    else {
+        inputElement.value += element; // Append the clicked button's value
+    }
+    blinkedinbox(inputElement)
+}
+
+// Select all elements with the class name "input"
+function blinkedinbox(inputElement){
+    inputElement.style.backgroundColor = 'red';
+    setTimeout(() => {
+        inputElement.style.backgroundColor = '#fff';  // Reset to original color
+    }, 300);
+   
+}
+
+function change() {
+    const inputElement = document.querySelector(".input").value; 
+    const outputElement = document.querySelector(".output"); 
+    
+    // Update the output area with the current input value
+    outputElement.textContent = inputElement;
+}
+
+
+
