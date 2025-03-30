0f3e94e932d20a5a01dc9d1a076995eb86228d65
SrinivasBootlabs <srinivas.kannan@bootlabstech.com>
2025-03-30 21:42:08 +0530

    changin

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+from httpd:2.4
+copy . /usr/local/apache2/htdocs/
--- scripts/postScript.sh
+++ scripts/postScript.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Restart the Apache server
-sudo systemctl start apache2
-# Clean up
-rm -rf /tmp/deployment_temp
+sudo systemctl restart apache2
